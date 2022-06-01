064652040d8f0f88b16530f7a2877949cf78c77d
tanganke <tang.anke@foxmail.com>
2022-06-01 16:37:51 +0800

    update

---
+++ pyutils/base/re.py
@@ -0,0 +1,48 @@
+import re
+
+def re_tuple(*elements):
+    R"""
+    Args:
+        elements: Regular expressions
+
+    Returns:
+        str: Regular expression matching a `tuple` of elements
+    """
+    return '\(\s*' + R',\s*'.join(elements) + (',\s*\)' if len(elements) <= 1 else '\s*\)')
+
+
+def re_list(*elements):
+    R"""
+    Args:
+        elements: Regular expressions
+
+    Returns:
+        str: Regular expression matching a `list` of elements
+    """
+    return '\[\s*' + R',\s*'.join(elements) + '\s*\]'
+
+
+def re_from_expr(x):
+    R"""
+    Example:
+        >>> re_int = '-?[0-9]+'
+        >>> re_pattern = re_from_expr(((re_int, (re_int,)), (re_int, (re_int,)), (re_int,)))
+        >>> re.match(re_pattern, '((-2146, (6,)), (1124, (97,)), (-1,))')
+        <re.Match object; span=(0, 37), match='((-2146, (6,)), (1124, (97,)), (-1,))'>
+
+    Args:
+        x: Python object
+    
+    Returns:
+        str: Regular expression
+    """
+    if isinstance(x, str):
+        return x
+    elif isinstance(x, tuple):
+        return re_tuple(*tuple(map(re_from_expr, x)))
+    elif isinstance(x, list):
+        return re_list(*tuple(map(re_from_expr, x)))
+    else:
+        raise NotImplementedError(
+            're_from_expr: not implemented for type {}'.format(type(x)))
+
