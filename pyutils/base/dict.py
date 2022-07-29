f4bef588cf236c302222f2a27fd65166f5879509
tanganke <tang.anke@foxmail.com>
2022-07-29 16:11:41 +0800

    add dict_get

--- pyutils/base/dict.py
+++ pyutils/base/dict.py
@@ -1,8 +1,12 @@
 from copy import deepcopy
+from typing import Tuple, List, Iterable, Union
 
-__all__ = ['dict_map']
 
-def dict_map(f, d: dict, *, max_level: int = -1, skip_levels = 0, inplace = False):
+def dict_get(d: dict, keys: Iterable[str], default=None):
+    return [d.get(k, default) for k in keys]
+
+
+def dict_map(f, d: dict, *, max_level: int = -1, skip_levels=0, inplace=False):
     """对d的每一个元素作用f，并返回一个新字典
 
     Args:
@@ -11,7 +15,7 @@ def dict_map(f, d: dict, *, max_level: int = -1, skip_levels = 0, inplace = Fals
         max_level (int, optional): 作用的深度， -1 表示无限深. Defaults to -1.
         skip_levels (int, optional): 跳过的层数. Defaults to 0.
         inplace (bool, optional): Defaults to False.
-    
+
     Returns:
         dict: 转换后的字典
     """
@@ -28,7 +32,7 @@ def dict_map(f, d: dict, *, max_level: int = -1, skip_levels = 0, inplace = Fals
             return
         for k in from_dict.keys():
             if isinstance(from_dict[k], dict):
-                dict_map_impl(from_dict[k], to_dict[k], level+1)
+                dict_map_impl(from_dict[k], to_dict[k], level + 1)
             else:
                 if level < skip_levels:
                     continue
